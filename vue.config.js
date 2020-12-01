f56837159fd7b357dd8a842b422b2b788ddd290d
Liou-q <liou666@126.com>
2020-12-01 09:07:36 +0800

    配置跨域代理

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,33 @@
 module.exports = {
-    configureWebpack: {
-      resolve: {
-        alias: {
-          'components': '@/components',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
         //   'content': 'components/content',
         //   'common': 'components/common',
-        'common':'@/common',
-          'assets': '@/assets',
-          'network': '@/network',
-          'views': '@/views',
-        }
-      }
-    }
-  }
+        common: "@/common",
+        assets: "@/assets",
+        network: "@/network",
+        views: "@/views",
+      },
+    },
+  },
+  devServer: {
+    host: "localhost",
+    port: 8080, // 端口号
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器
+    proxy: {
+      "/admin": {
+        //代理api
+        target: "http://ceshi3.dishait.cn/admin", //服务器api地址
+        ws: true, // proxy websockets
+        changeOrigin: true, //是否跨域
+        pathRewrite: {
+          //重写路径
+          "^/admin": "",
+        },
+      },
+    },
+  },
+};
